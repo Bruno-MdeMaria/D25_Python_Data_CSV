040f56e4b726445ee610fb6473d15f7bc7813758
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-10-04 01:27:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import csv
 with open("weather_data.csv") as data_value:
     data = csv.reader(data_value)
+    temperaturas = []
     for linha in data:
-        print(linha)
+       if linha[1] != "temp":  #necessário esta condição apra não puxar a palavra "temp" que está na coluna températuras e puxar somente os valores.
+        temperaturas.append(linha[1])
+    print(temperaturas)
+           
